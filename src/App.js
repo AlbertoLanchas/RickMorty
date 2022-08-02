f9b4d7d380b68c1e50bcec75d467052668f303cc
AlbertoLanchas <albertolsn7@gmail.com>
2022-08-02 11:10:13 +0200

    links created
    Signed-off-by: AlbertoLanchas <albertolsn7@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Home from "./components/Home";
 import Characters from "./components/Characters";
+import CharacterDetails from "./components/CharacterDetails";
 import Episodes from "./components/Episodes";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavLink to={`/home`} key={"home"}>
+          HOME
+        </NavLink>
+        <NavLink to={`/characters`} key={"characters"}>
+          CHARACTERS
+        </NavLink>
+        <NavLink to={`/episodes`} key={"episodes"}>
+          EPISODES
+        </NavLink>
         <Routes>
-          <Route path="/home" element={Home}></Route>
-          <Route path="/characters" element={Characters}></Route>
-          <Route path="/characters/:id" element={Characters}></Route>
-          <Route path="/episodes" element={Episodes}></Route>
+          <Route path="/home" element={<Home></Home>}></Route>
+          <Route path="/characters" element={<Characters></Characters>}></Route>
+          <Route
+            path="/characters/:id"
+            element={<CharacterDetails></CharacterDetails>}
+          ></Route>
+          <Route path="/episodes" element={<Episodes></Episodes>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
