bf643962dfec8906e6ba4f860295b9c1b594a2f9
AlbertoLanchas <albertolsn7@gmail.com>
2022-08-04 18:56:39 +0200

    episodes fetch data completed
    Signed-off-by: AlbertoLanchas <albertolsn7@gmail.com>

--- src/Pages/Episodes.jsx
+++ src/Pages/Episodes.jsx
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Cards from "../components/Pure/Cards/Cards";
+
 import "../styles/Episodes.css";
 
 const Episodes = () => {
-  return <div>Episodes</div>;
+  const [id, setID] = useState(1);
+  const [info, setInfo] = useState([]);
+  const [results, setResults] = useState([]);
+  let { air_date, name } = info;
+  let api = `https://rickandmortyapi.com/api/episode/${id}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      setInfo(data);
+      let a = await Promise.all(
+        data.characters.map((x) => {
+          return fetch(x).then((res) => res.json());
+        })
+      );
+      setResults(a);
+    })();
+  }, [api]);
+  return (
+    <div className="container">
+      <div className="row mb-4">
+        <h2 className="text-center mb-3">
+          Episode:{" "}
+          <span className="text-primary">{name === "" ? "Unknown" : name}</span>
+        </h2>
+        <h5 className="text-center">
+          Air Date:{air_date === "" ? "Unknown" : air_date}
+        </h5>
+      </div>
+      <div className="row">
+        <div class="col-3">Pick Episodes</div>
+        <div className="col-8">
+          {" "}
+          <div class="row">
+            <Cards results={results} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Episodes;
