7e8bc6f46a0a8a6861509b9532940e111cc1888f
AlbertoLanchas <albertolsn7@gmail.com>
2022-10-06 19:55:49 +0200

    scroll bar add
    Signed-off-by: AlbertoLanchas <albertolsn7@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import React, { useEffect, useState } from "react";
-import NavigationBar from "./NavigationBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { NavLink } from "react-router-dom";
 import Home from "./Pages/Home";
 import Characters from "./Pages/Characters";
 import CharacterDetails from "./Pages/CharacterDetails";
 import Episodes from "./Pages/Episodes";
 import Empty from "./Pages/Empty";
 import "./App.css";
-import Filters from "./components/Pure/Filters/Filters";
-import NavBar from "./components/NavBar/NavBar";
 import Location from "./Pages/Location";
 
 function App() {
